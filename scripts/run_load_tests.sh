2536d336d88fe06398af9907dce6c25ea934f249
m0neZ <ramonkattan@gmail.com>
2025-07-21 14:59:30 -0300

    Add k6 load testing

--- scripts/run_load_tests.sh
+++ scripts/run_load_tests.sh
@@ -9,4 +9,9 @@ locust -f load_tests/locustfile.py \
   -r "${SPAWN_RATE:-5}" \
   --run-time "${RUN_TIME:-1m}"
 
+# Run the k6 script against the API gateway
+k6 run \
+  --env RESULTS_PATH="${K6_RESULTS:-load_tests/k6_results.json}" \
+  load_tests/k6_api_gateway.js
+
 pytest -W error backend/mockup-generation/tests/test_performance.py::test_concurrent_generation_gpu_utilization "$@"

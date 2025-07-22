1e6346a13cdd3bd9abadd9e3fdf35fc10b1298d3
m0neZ <ramonkattan@gmail.com>
2025-07-22 16:29:46 -0300

    chore(flow): annotate JS modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,15 @@ module.exports = {
     'standard',
     'prettier',
   ],
+  plugins: ['flowtype'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: ['@babel/plugin-syntax-flow'],
+    },
   },
   settings: {
     react: {
--- frontend/admin-dashboard/public/sw.js
+++ frontend/admin-dashboard/public/sw.js
@@ -1,4 +1,8 @@
-self.addEventListener('message', (event) => {
+// @flow
+
+declare var self: mixed;
+
+self.addEventListener('message', (event: MessageEvent<mixed>): void => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
